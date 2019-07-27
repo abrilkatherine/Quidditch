d0fc8c39a7dabc917d3ff20822ba2218f2f8d7dd
Abril <abrilkatherine@hotmail.com>
2019-07-26 21:44:13 -0300

    agregamos metodos del punto 1.e

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,8 +1,30 @@
 package main.java;
 
+import java.util.Calendar;
+
 public class Nimbus implements Escoba {
 
-    public Double velocidadEscoba(){
+    private Double salud= 70.00;
+    private Integer añoDeFabricacion= 2010;
 
+    public Double velocidadEscoba(){
+        return (80- (Calendar.getInstance().get(Calendar.YEAR)-añoDeFabricacion))*salud/100;
+
+    }
+
+    public Double getSalud() {
+        return salud;
+    }
+
+    public void setSalud(Double salud) {
+        this.salud = salud;
+    }
+
+    public Integer getAñoDeFabricacion() {
+        return añoDeFabricacion;
+    }
+
+    public void setAñoDeFabricacion(Integer añoDeFabricacion) {
+        this.añoDeFabricacion = añoDeFabricacion;
     }
 }
--- src/main/java/SaetaDeFuego.java
+++ src/main/java/SaetaDeFuego.java
@@ -3,6 +3,6 @@ package main.java;
 public class SaetaDeFuego implements Escoba {
 
     public Double velocidadEscoba(){
-
+        return 100.00;
     }
 }
