06228fca5ecff5183004dd9a804a8137ef52e013
Abril <abrilkatherine@hotmail.com>
2019-08-21 16:59:46 -0300

    arreglo de bugs

--- src/test/java/CazadorTest.java
+++ src/test/java/CazadorTest.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 public class CazadorTest {
